9e036e7e31eb6f92560a95b1c9d78f44603b6f4a
unknown <42840614+lustat@users.noreply.github.com>
2019-09-15 16:43:21 +0200

    create a window

--- src/test1.py
+++ src/test1.py
@@ -1,1 +1,20 @@
 import arcade
+
+
+if __name__ == '__main__':
+    print('Demo to set up first test game')
+
+    # Open the window. Set the window title and dimensions (width and height)
+    arcade.open_window(600, 600, "Explosion game")
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.start_render()
+
+    # Finish the render.
+    # Nothing will be drawn without this.
+    # Must happen after all draw commands
+    arcade.finish_render()
+
+    # Keep the window up until someone closes it.
+    arcade.run()
+
+    print('Finished')
